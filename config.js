5d599fec222482f0b88e5336e333ef9273825e4d
denisputrans <32485140+denisputrans@users.noreply.github.com>
2019-11-21 16:55:05 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    redisOpForDice  : {host:'188.225.73.246',port:'3000',password:'cV6Kxufu',db:3},
+    redisOpForRing  : {host:'188.225.73.246',port:'3000',password:'cV6Kxufu',db:4},
+    redisOpForTower : {host:'188.225.73.246',port:'3000',password:'cV6Kxufu',db:5},
+    trongridApi : 'https://api.trongrid.io',
+    privateKey : '29545718406ABE1361F1832D83CBD3C172ECD7336550AE6AA94D81C8838B44D2',// TB1zCK6zJFHRbdnWqgq3hw1DqWVbNA4fzW //my TV4aSLBugaxGDuctxYLCixeoj6UsBecZav
+    PRIVATE_KEY_ACT : '9E54D4DF686FE81E91C3FE3053F7CE15A9B997B901E246FFE8A87825EBCBE408', //TTGysgU75PGBp1JV5c2b6cjf2pc3Qo1V5i //my TXXkdFXBG7dQZVjgFCW9MQSSx9SYTWpVyM
+    PRIVATE_KEY_ACT2 : '213D4A108F47C9873CA5BC41819B9B480E80170E39840AF6066F699B0E961726', //TDt5hWPLGJgbjnpW629Xx3uYem45Tjg5Ez  //my TLCBPcsYzNnvKi3CYyspM8uJhruKzbUK3K
+  };
+
+  module.exports = config;
